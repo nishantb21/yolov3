1cba09f89e42755ae92924b4b4c0ae797e6b12d0
Nishant Bhattacharya <bnishant@iisc.ac.in>
2019-11-24 22:20:40 +0530

    Added the basic DarkNet53 model and the conda environment file

---
+++ core/common.py
@@ -0,0 +1,56 @@
+import torch
+
+class Dense(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Dense, self).__init__()
+        self.dense = torch.nn.Linear(in_channels, out_channels)
+
+    def forward(self, inp):
+        return self.dense(inp)
+
+class Flatten(torch.nn.Module):
+    def __init__(self):
+        super(Flatten, self).__init__()
+
+    def forward(self, inp):
+        return torch.flatten(inp, start_dim=1)
+
+class Convolution(torch.nn.Module):
+    ''' 
+        Convolutional Module with Batchnormalzation and Activation
+    '''
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding="same", activation="leaky"):
+        super(Convolution, self).__init__()
+        if padding == "same":
+            padding = kernel_size // 2
+
+        self.conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding)
+        self.bn = torch.nn.BatchNorm2d(out_channels)
+        if activation == "linear":
+            self.activation = torch.nn.Identity()
+        elif activation == "leaky":
+            self.activation = torch.nn.LeakyReLU()
+
+    def forward(self, inp):
+        x = self.conv(inp)
+        x = self.bn(x)
+        x = self.activation(x)
+
+        return x
+
+class ResidualBlock(torch.nn.Module):
+    def __init__(self, in_channels, channels_1, channels_2):
+        super(ResidualBlock, self).__init__()
+        self.conv_1 = Convolution(in_channels, channels_1, 1, 1)
+        self.conv_2 = Convolution(channels_1, channels_2, 3, 1)
+
+    def forward(self, inp):
+        x = self.conv_1(inp)
+        x = self.conv_2(x)
+        x += inp
+
+        return x
+
+def residual_repeater(number_repetitions, in_channels, channels_1, channels_2):
+    layer = [ResidualBlock(in_channels, channels_1, channels_2) for i in range(number_repetitions)]
+    return torch.nn.Sequential(*layer)
---
+++ core/models.py
@@ -0,0 +1,41 @@
+import torch
+from common import Convolution, Flatten, Dense, residual_repeater
+
+class DarkNet53(torch.nn.Module):
+    def __init__(self, height, width, channels, classes):
+        super(DarkNet53, self).__init__()
+        downsampled_height = height // 32
+        downsampled_width = width // 32
+
+        self.conv_1 = Convolution(channels, 32, 3, 1)
+        self.conv_2 = Convolution(32, 64, 3, 2)
+        self.repeater_1 = residual_repeater(1, 64, 32, 64)
+        self.conv_3 = Convolution(64, 128, 3, 2)
+        self.repeater_2 = residual_repeater(2, 128, 64, 128)
+        self.conv_4 = Convolution(128, 256, 3, 2)
+        self.repeater_3 = residual_repeater(8, 256, 128, 256)
+        self.conv_5 = Convolution(256, 512, 3, 2)
+        self.repeater_4 = residual_repeater(8, 512, 256, 512)
+        self.conv_6 = Convolution(512, 1024, 3, 2)
+        self.repeater_5 = residual_repeater(4, 1024, 512, 1024)
+        self.avg_pool = torch.nn.AvgPool2d((downsampled_height, downsampled_width))
+        self.flatten = Flatten()
+        self.final_cls = Dense(1024, classes)
+
+    def forward(self, inp):
+        x = self.conv_1(inp)
+        x = self.conv_2(x)
+        x = self.repeater_1(x)
+        x = self.conv_3(x)
+        x = self.repeater_2(x)
+        x = self.conv_4(x)
+        x = self.repeater_3(x)
+        x = self.conv_5(x)
+        x = self.repeater_4(x)
+        x = self.conv_6(x)
+        x = self.repeater_5(x)
+        x = self.avg_pool(x)
+        x = self.flatten(x)
+        x = self.final_cls(x)
+
+        return x
